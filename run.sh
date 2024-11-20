54d3379f1c41128adb46cf33fb019ffda1aaf578
Wenzhe Liu (刘文哲) <liuwenzhe19@mails.ucas.ac.cn>
2024-11-20 19:43:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # lora
 deepspeed --include localhost:1,2,3,4 --master_port 520 ./train_sft.py \
-                --data_path data.json \
+                --data_path data/data.json \
                 --per_device_train_batch_size 1 \
                 --max_len 128000 \
                 --is_skip \
@@ -15,7 +15,7 @@ deepspeed --include localhost:1,2,3,4 --master_port 520 ./train_sft.py \
                 --lora_dropout 0.1 \
                 --lora_module_name "query_key_value,dense_h_to_4h,dense_4h_to_h,dense" \
                 --seed 1234 \
-                --ds_file ds_zero2_no_offload.json \
+                --ds_file ds_config/ds_zero2_no_offload.json \
                 --gradient_checkpointing \
                 --show_loss_step 10 \
                 --save_model_step 200 \
@@ -23,7 +23,7 @@ deepspeed --include localhost:1,2,3,4 --master_port 520 ./train_sft.py \
 
 # 全量sft
 # deepspeed --include localhost:1,2,3,4 --master_port 520 ./train_sft.py \
-#               --train_path data.json \
+#               --train_path data/data.json \
 #               --per_device_train_batch_size 1 \
 #               --max_len 128000 \
 #               --learning_rate 1e-4 \
@@ -33,7 +33,7 @@ deepspeed --include localhost:1,2,3,4 --master_port 520 ./train_sft.py \
 #               --warmup_ratio 0.1 \
 #               --train_type all \
 #               --seed 1234 \
-#               --ds_file ds_zero2_no_offload.json \
+#               --ds_file ds_config/ds_zero2_no_offload.json \
 #               --show_loss_step 10 \
 #               --save_model_step 200 \
 #               --output_dir out
